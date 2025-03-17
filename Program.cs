c5d448935ae2239a8f0ca0458f9a682998e65dda
Alix Gianessi <alix.zarude@gmail.com>
2025-03-17 10:31:32 +0100

    Ajout de la saisie clavier

--- Program.cs
+++ Program.cs
@@ -3,5 +3,6 @@
     return (minute/60, minute%60);
 }
 
-(int H, int M) = heure_minute(130);
+int minute_non_converti = Convert.ToInt32(Console.ReadLine());
+(int H, int M) = heure_minute(minute_non_converti);
 Console.WriteLine($"{H}:{M}");
